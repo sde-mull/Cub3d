7596ebd3cc7be7cf4f01f4f1ea2350d08bdd7108
Sebastiao Muller <sebastiaomuller@MacBook-Air-de-Sebastiao.local>
2023-03-01 20:02:18 +0000

    update

--- cub3d.h
+++ cub3d.h
@@ -1,33 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sde-mull <sde.mull@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/29 11:40:56 by pcoimbra          #+#    #+#             */
-/*   Updated: 2023/03/01 16:05:49 by sde-mull         ###   ########.fr       */
+/*   Created: 2023/03/01 16:06:38 by sde-mull          #+#    #+#             */
+/*   Updated: 2023/03/01 16:29:37 by sde-mull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef CUB3D_H
+# define CUB3D_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <string.h>
+# include "libft/libft.h"
+# include "gnl/get_next_line.h"
 # include <stdlib.h>
-# include <ctype.h>
-# include <stdbool.h>
+# include <unistd.h>
 # include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <errno.h>
 
-# define BUFFER_SIZE 42 
-
-char		*ft_strdup(const char *s1);
-char		*ft_strchr(const char *s, int c);
-size_t		ft_strlcpy(char *dest, const char *src, size_t dstsize);
-size_t		ft_strlen(const char *s1);
-char		*ft_strjoin(char const *s1, char const *s2);
-char		*get_next_line(int fd);
+int check_input(int n, char *file);
 
 #endif
