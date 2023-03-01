3ca15977122976a1170c73bf504396578bb6f73a
Sebastiao Muller <sebastiaomuller@MacBook-Air-de-Sebastiao.local>
2023-03-01 21:15:07 +0000

    starting point

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: sde-mull <sde.mull@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 16:06:38 by sde-mull          #+#    #+#             */
-/*   Updated: 2023/03/01 16:29:37 by sde-mull         ###   ########.fr       */
+/*   Updated: 2023/03/01 20:43:32 by sde-mull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,8 @@
 # include <errno.h>
 
 int check_input(int n, char *file);
+int	check_file(char *file);
+int	check_extension(char *file);
+int	ft_str_cmp(char *file, char *str);
 
 #endif
